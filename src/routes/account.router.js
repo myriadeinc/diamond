36e6a5f9f05bb6b5c29546a42ffbbc4c30cfcd14
Han Xiao <yhxiao97@gmail.com>
2019-09-01 19:43:24 -0400

    IS02 Finished the login flow with JWT token

--- src/routes/account.router.js
+++ src/routes/account.router.js
@@ -8,18 +8,9 @@ const { check } = require('express-validator');
 const AccountService = require('src/services/accounts.service.js');
 const EmailVerificationMiddleware = require('src/middleware/email.validation.middleware.js');
 const RequestValidationMiddleware = require('src/middleware/request.validation.middleware.js');
+const AuthMiddleware = require('src/middleware/auth.middleware.js');
 
-
-router.get(`/:accountId`, 
-  (req, res) => {
-    return AccountService.getAccount(req.params.accountId)
-      .then((acc) => {
-        res.status(200).send(acc.toJSON());
-      })
-      .catch((err) => {
-        res.sendStatus(404);
-      });
-});
+const TokenService = require('src/services/token.service.js');
 
 router.post('/create', 
   [
@@ -45,7 +36,18 @@ router.post('/login',
   ],
   RequestValidationMiddleware.handleErrors,
   (req, res, next) => {
-    
+    return AccountService.validatePassword(req.body.email, req.body.password)
+    .then(acc => {
+      return TokenService.createAccessToken(acc);
+    })
+    .then(tok => {
+      res.status(200).send({
+        accessToken: tok
+      })
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    })
   }
 );
 
@@ -56,26 +58,41 @@ router.post('/address-login',
   ]
 )
 
-// These routes will be disabled until 
-// router.put(`/:accountId`, (req, res) => {
-//   return AccountService.updateAccount(req.params.accountId, req.body)
-//       .then((acc) => {
-//         res.status(200).send(acc.toJSON());
-//       })
-//       .catch((err) => {
-//         res.sendStatus(500);
-//       });
-// });
+router.get(`/:accountId`, 
+  AuthMiddleware.authenticateUser,
+  (req, res) => {
+    return AccountService.getAccount(req.accountId)
+      .then((acc) => {
+        res.status(200).send(acc.toJSON());
+      })
+      .catch((err) => {
+        res.sendStatus(404);
+      });
+});
 
-// router.delete(`/:accountId`, (req, res) => {
-//   return AccountService.deleteAccount(req.params.accountId)
-//       .then(() => {
-//         res.sendStatus(200);
-//       })
-//       .catch((err) => {
-//         res.sendStatus(404);
-//       });
-//});
+router.put(`/:accountId`, 
+  AuthMiddleware.authenticateUser,
+  (req, res) => {
+    return AccountService.updateAccount(req.accountId, req.body)
+        .then((acc) => {
+          res.status(200).send(acc.toJSON());
+        })
+        .catch((err) => {
+          res.sendStatus(500);
+        });
+});
+
+router.delete(`/:accountId`, 
+  AuthMiddleware.authenticateUser,
+  (req, res) => {
+    return AccountService.deleteAccount(req.accountId)
+        .then(() => {
+          res.sendStatus(200);
+        })
+        .catch((err) => {
+          res.sendStatus(404);
+        });
+});
 
 
 module.exports = router;
