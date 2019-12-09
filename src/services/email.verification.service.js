843d5ee1408aabc3e7dc856d55cffde1ce93c9d8
Han Xiao <yhxiao97@gmail.com>
2019-12-08 19:55:34 -0500

    changing email signup flow

--- src/services/email.verification.service.js
+++ src/services/email.verification.service.js
@@ -23,13 +23,30 @@ const EmailVerificationService = {
       await cache.put(tok, email, 'Email::Confirmation');
       await mailer.send(email, {
         subject: 'Confirm your email',
-        text: `Thank you for confirming your email with Myriade. 
-        Here your email confirmation token used for creating an account:
-        ${tok} Thank you.`,
+        text: `Thank you for registrating your e-mail on Myriade. Please click on the link below to confirm your e-mail and proceed with account creation: 
+        https://myriade.io/#/signup?token=${tok}
+        
+        If you did not request an account creation on Myriade, feel free to ignore this e-mail
+        Thank you!`,
         html: `
         <div>
-          <h4>Thank you for confirming your email with Myriade. </h4>
-          <p>Here your email confirmation token used for creating an account: <strong>${tok} </strong></p>
+          <h5>Thank you for registrating your e-mail on Myriade. Please click on the link below to confirm your e-mail and proceed with account creation</h5>
+          <p>If you did not request an account creation on Myriade, feel free to ignore this e-mail</p>
+          <table width="100%" cellspacing="0" cellpadding="0">
+            <tr>
+                <td>
+                    <table cellspacing="0" cellpadding="0">
+                        <tr>
+                            <td style="border-radius: 2px;" bgcolor="#DE5100">
+                                <a href="https://myriade.io/#/signup?token=${tok}" target="_blank" style="padding: 8px 12px; border: 1px solid #DE5100;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">
+                                    Confirm your e-mail       
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+          </table>
           <p>Thank you!</p>
         </div>
         `
