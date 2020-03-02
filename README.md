163b8d17709327e5944c77d6a7b056ca832b9533
Jonathan <jonathanxu25@gmail.com>
2020-03-01 20:08:17 -0500

    working on timer

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-## Diamond 
+## Diamond
 
 Identity service for Myriade mining pool. Single source of truth in miner accounts and authentication for cloud services.
 
 [![CircleCI](https://circleci.com/gh/myriadeinc/diamond/tree/master.svg?style=svg)](https://circleci.com/gh/myriadeinc/diamond/tree/master)
+
+## Local Build
+
+Ports: 8180, 9873
--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,29 +1,32 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
-const jwt = require('jsonwebtoken');
-const uuid = require('uuid');
-const config = require('src/util/config.js');
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const uuid = require("uuid");
+const config = require("src/util/config.js");
 
 const TokenService = {
-
   encodeAndSign: (data, options) => {
-    return jwt.sign(data, config.get('jwt:private_key'), _.extend({}, options, {
-      algorithm: config.get('jwt:algorithm'),
-      jwtid: uuid.v4(),
-      issuer: config.get('service:name'),
-    }));
+    return jwt.sign(
+      data,
+      config.get("jwt:private_key"),
+      _.extend({}, options, {
+        algorithm: config.get("jwt:algorithm"),
+        jwtid: uuid.v4(),
+        issuer: config.get("service:name"),
+      })
+    );
   },
 
   decodeAndVerify: (token) => {
     return new Promise((resolve, reject) => {
       let decodedToken;
       try {
-        decodedToken = jwt.verify(token, config.get('jwt:public_key'));
+        decodedToken = jwt.verify(token, config.get("jwt:public_key"));
       } catch (err) {
         return reject(err);
       }
-      return resolve(_.omit(decodedToken, ['iat', 'jti']));
+      return resolve(_.omit(decodedToken, ["iat", "jti"]));
     });
   },
 
@@ -31,18 +34,20 @@ const TokenService = {
     // TODO: Fix error where dividing by 1000 to get unix in seconds does not actually give right value
     let expiry = Date.now() + 1000 * 60 * 60;
     //expiry = Math.floor(expiry / 1000);
-    return TokenService.encodeAndSign({
-      version: 1,
-      account: {
-        email: account.email,
-        name: account.name,
-        address: account.wallet_address,
+    return TokenService.encodeAndSign(
+      {
+        version: 1,
+        account: {
+          email: account.email,
+          name: account.name,
+          address: account.wallet_address,
+        },
       },
-
-    }, {
-      expiresIn: expiry,
-      subject: account.id,
-    });
+      {
+        expiresIn: expiry,
+        subject: account.id,
+      }
+    );
   },
 };
 
